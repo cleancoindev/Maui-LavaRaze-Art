a73663c8bc87f02ba74fc7db8bcba8a3c52dc096
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-08-11 10:08:01 -1000

    Initial commit

---
+++ src/consts/parameters.ts
@@ -0,0 +1,22 @@
+/** Change these values to configure the application for your own use. **/
+
+// Your smart contract address (available on the thirdweb dashboard)
+// For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
+export const contractConst = "0x851c7B9e99dfdCA51468636C53d51fDb36Aa3363";
+
+// The name of the chain your contract is deployed to.
+// Provide a string for standard chains, or import one from @thirdweb-dev/chains package.
+// Learn more: https://portal.thirdweb.com/react/react.thirdwebprovider#activechain-recommended
+export const chainConst = "polygon";
+
+// Configure the primary color for buttons and other UI elements
+export const primaryColorConst = "purple";
+
+// Choose between "light" and "dark" mode
+export const themeConst = "light";
+
+// Gasless relayer configuration options
+export const relayerUrlConst = ""; // OpenZeppelin relayer URL
+export const biconomyApiKeyConst = ""; // Biconomy API key
+export const biconomyApiIdConst = ""; // Biconomy API ID
+export const clientIdConst = "5b4430e33aa895c297254791cfcad593"; // Client ID
